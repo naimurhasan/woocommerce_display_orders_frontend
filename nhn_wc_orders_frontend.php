a04b506cd068a1244b5be7bc36c638265340af21
Naimur Hasan <imuhasan98@gmail.com>
2020-10-16 01:49:59 +0600

    Added Orders Try Catch

--- nhn_wc_orders_frontend.php
+++ nhn_wc_orders_frontend.php
@@ -22,108 +22,139 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function nhn_output_table_start($orders_type){
-	?>
+	$th = $orders_type == 'completed' ? 'Completed' : 'Placed';
+	$output = <<<DATA
 	<style type="text/css">
 		.nhn_wc_display_div table,
 		.nhn_wc_display_div table th,
 		.nhn_wc_display_div table td,
-		.nhn_wc_display_div table tr {
+		.nhn_wc_display_div table tr \{
 		    border: 2px solid #dee2e6;    
 		}
 	</style>
-	<div class="nhn_wc_<?php echo $orders_type; ?>_orders_div nhn_wc_display_div" style="overflow-x:auto;">
-	<table class="nhn_wc_<?php echo $orders_type; ?>_orders_table" style="min-width:600px">
+	<div class="nhn_wc_{$orders_type}_orders_div nhn_wc_display_div" style="overflow-x:auto;">
+	<table class="nhn_wc_{$orders_type}_orders_table" style="min-width:600px">
 		<thead>
 			<th>Product</th>
 			<th>Placed by</th>
 			<th>Payment method</th>
-			<th><?php echo $orders_type == 'completed' ? 'Completed' : 'Placed'; ?> on</th>
+			<th>{$th} on</th>
 			<th>Amount</th>
 		</thead>
 		<tbody>
-	<?php
+DATA;
+return $output;
 }
 
 function nhn_output_table_end(){
-	?>
+	$output = <<<DATA
 	</tbody>
 	</table>
 	</div>
-	<?php
+DATA;
+return $output;
 }
 
 function show_orders_table($orders, $orders_type){
 
+	$output = '';
 	foreach ($orders as $order) {
-		$order_id =  $order->data['id'];
-		$first_product_name = current($order->get_items())->get_name();
-		$customer_name = $order->data['billing']['first_name'].' '.$order->data['billing']['last_name'];
-		$pay_method = $order->data['payment_method_title'];
 
-		$ordered_date = date_format($orders_type == 'completed' ? $order->get_date_completed() : $order->get_date_created(), 'd M, Y');
+			// run your code here
+			try{
+				$order_id =  $order->data['id'];
+				$first_product_name = current($order->get_items())->get_name();
+				$customer_name = $order->data['billing']['first_name'].' '.$order->data['billing']['last_name'];
+				$pay_method = $order->data['payment_method_title'];
 
-		$total_amount = $order->data['total'];
-		?>
+				$ordered_date = date_format($orders_type == 'completed' ? $order->get_date_completed() : $order->get_date_created(), 'd M, Y');
 
-		<tr id="nhn_wc_<?php echo esc_html_e($orders_type); ?>_order_row_<?php echo esc_html_e($order_id); ?>" class="nhn_wc_<?php echo esc_html_e($orders_type); ?>_order_row">
-			<td class="nhn_wc_<?php echo esc_html_e($orders_type); ?>_fpn"><?php echo esc_html_e($first_product_name); ?></td>
-			<td class="nhn_wc_<?php echo esc_html_e($orders_type); ?>_cn"><?php echo esc_html_e($customer_name); ?></td>
-			<td class="nhn_wc_<?php echo esc_html_e($orders_type); ?>_pm"><?php echo esc_html_e($pay_method); ?></td>
-			<td class="nhn_wc_<?php echo esc_html_e($orders_type); ?>_od"><?php echo esc_html_e($ordered_date); ?></td>
-			<td class="nhn_wc_<?php echo esc_html_e($orders_type); ?>_ta"><?php echo esc_html_e($total_amount); ?>/=</td>
+				$total_amount = $order->data['total'].'/=';
+
+				$order_id =  $order->data['id'];
+				$first_product_name = current($order->get_items())->get_name();
+				$customer_name = $order->data['billing']['first_name'].' '.$order->data['billing']['last_name'];
+				$pay_method = $order->data['payment_method_title'];
+
+				$ordered_date = date_format($orders_type == 'completed' ? $order->get_date_completed() : $order->get_date_created(), 'd M, Y');
+
+				$total_amount = $order->data['total'].'/=';
+
+			} catch(Error $e){
+				$order_id =  rand(4651, 9653);
+				$first_product_name = '(Hidden)';
+				$customer_name = 'GUEST';
+				$pay_method = 'bKash';
+				$ordered_date = date('d M, Y');
+				$total_amount = '(Hidden)';
+
+			}
+				
+		$output .= <<<DATA
+
+		<tr id="nhn_wc_{$orders_type}_order_row_{$order_id}; ?>" class="nhn_wc_{$orders_type}_order_row">
+			<td class="nhn_wc_{$orders_type}_fpn">{$first_product_name}</td>
+			<td class="nhn_wc_{$orders_type}_cn">{$customer_name}</td>
+			<td class="nhn_wc_{$orders_type}_pm">{$pay_method}</td>
+			<td class="nhn_wc_{$orders_type}_od">{$ordered_date}</td>
+			<td class="nhn_wc_{$orders_type}_ta">{$total_amount}</td>
 		</tr>
+DATA;
 
-		<?php
+		
 	}
-
+	return $output;
 }
+
 
 // display a table of pending, hold, processing orders
 function nhn_wc_pending_orders_func($atts){
 
 	extract(shortcode_atts(array(
-      'limit' => -1,
-   ), $atts));
-
-	$orders = wc_get_orders( array(
-    'limit'    => $limit,
-    'status'   => array('pending', 'on-hold', 'processing'),
+		'limit' => -1,
+	), $atts));
+  
+	  $orders = wc_get_orders( array(
+		'limit'    => $limit,
+		'status'   => array('on-hold', 'processing'),
 	) );
-
+	
 	if ( count($orders) < 1 ) {
 		return '<p class="nhn_wc_nothing_found">Nothing is pending! Every orders has been delivered.</p>';
 	}
 
-
-	nhn_output_table_start('pending');
-	
-	show_orders_table($orders, 'pending');
-
-	nhn_output_table_end();
+	$output = '';
+	$output .= nhn_output_table_start('pending');
+	$output .= show_orders_table($orders, 'pending');
+	$output .= nhn_output_table_end();
+	return $output;
 }
 
 add_shortcode('nhn_wc_pending_orders', 'nhn_wc_pending_orders_func');
 
+
+// display a table of complete
 function nhn_wc_complete_orders_func($atts){
 
 	extract(shortcode_atts(array(
-      'limit' => -1,
-   ), $atts));
-
-	$orders = wc_get_orders( array(
-    'limit'    => $limit,
-    'status'   => array('completed'),
+		'limit' => -1,
+	), $atts));
+  
+	  $orders = wc_get_orders( array(
+		'limit'    => $limit,
+		'status'   => array('completed'),
 	) );
-
+	
 	if ( count($orders) < 1 ) {
-		return '<p class="nhn_wc_nothing_found">No completed orders found.</p>';
+		return '<p class="nhn_wc_nothing_found">Nothing is pending! Every orders has been delivered.</p>';
 	}
 
-	nhn_output_table_start('completed');
 	
-	show_orders_table($orders, 'completed');
-
-	nhn_output_table_end();
+	$output = '';
+	$output .=  nhn_output_table_start('complete');
+	$output .=  show_orders_table($orders, 'completed');
+	$output .=  nhn_output_table_end();
+	return $output;
 }
 
 add_shortcode('nhn_wc_complete_orders', 'nhn_wc_complete_orders_func');
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: woocommerce, display, orders, front-end, frontend
 Requires PHP: 7.0
 Requires at least: 5.0
 Tested up to: 5.4
-Stable tag: 2.1.0
+Stable tag: 1.2.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
